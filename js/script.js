0deafb99d30851d865dd33e8fe1c205716018aa7
Gauthier Boe <gaurraka@gmail.com>
2025-05-20 02:16:47 +0200

    init good

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+function textScrambled(event, txt) {
+
+    // Clear any existing interval for this element
+    if (intervals[txt]) {
+        clearInterval(intervals[txt]);
+    }
+
+    let iteration = 0;
+
+    intervals[txt] = setInterval(() => {
+        event.innerText = event.innerText
+            .split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return txt[index];
+                }
+                return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+
+        if (iteration >= txt.length) {
+            clearInterval(intervals[txt]);
+            delete intervals[txt];
+        }
+
+        iteration += 1 / 3;
+    }, 20);
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////// Formation ////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -15,14 +44,13 @@ var idLangue = 0;
 
 // Ecriture dans le temrinal
 let formation;
-progEncours.style.visibility = "visible";
+progEncours.style.visibility = "hidden";
 
 let progress = 0;
 let speed = 10; // Vitesse de frappe (en millisecondes par caractère)
 
 let phrase;
 let char;
-
 
 function progression(val) {
 
@@ -52,6 +80,9 @@ function progression(val) {
 }
 
 function typeText() {
+
+
+    progEncours.style.visibility = "visible";
 
     // On écrit le répertoire / titre
     if (progress == 0) {
@@ -99,3 +130,56 @@ function afficherTailleBloc() {
 const observer2 = new ResizeObserver(afficherTailleBloc);
 observer2.observe(progressionBloc);
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////// Experiences ///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.querySelectorAll(".pixelTab div img").forEach(element => {
+    var random = Math.floor(Math.random() * 20);
+    element.style.width = (random + 20) + "vw"; // Set the width as a string with the unit
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////// Projets /////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function createGrid() {
+//     console.log("createGrid")
+//     nbRowPixel = Math.floor(windowHeight / 50);
+//     nbColPixel = Math.floor(windowWidth / 50);
+
+//     heightPixel = windowHeight / nbRowPixel;
+//     widthPixel = windowWidth / nbColPixel;
+
+//     console.log(nbRowPixel, heightPixel, nbColPixel, widthPixel)
+
+//     for (i = 0; i < nbRowPixel; i++) {
+//         for (j = 0; j < nbColPixel; j++) {
+//             document.getElementById("transitionProjet").innerHTML += `
+//                 <div class="pixel pixelWhite" style="width:${widthPixel}px;height:${heightPixel}px"></div>
+//                 `;
+//         }
+//     }
+
+//     console.log("end createGrid")
+// }
+// createGrid();
+
+// function animeGrid() {
+
+//     var pixels = document.querySelectorAll(".pixel.pixelWhite");
+
+//     if (pixels.length === 0) {
+//         return;
+//     }
+
+//     var alea = Math.floor(Math.random() * pixels.length);
+
+//     pixels[alea].classList.add("pixelBlack");
+//     pixels[alea].classList.remove("pixelWhite");
+
+//     if (pixels.length > 1) {
+//         setTimeout(animeGrid, 1);
+//     }
+// }
--- js/scriptGSAP.js
+++ js/scriptGSAP.js
@@ -1,12 +1,81 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(SplitText);
+
+window.addEventListener("scroll", reveal);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
+var ratioScreen = windowWidth / windowHeight;
+
+var dif = windowHeight - windowWidth;
+var ajustementCard = dif / 2;
+
+var rationImage = 2930 / 2327;
+var difRatio = ratioScreen / rationImage;
+
+var heightLengthFit = 0;
+var widthLengthFit = 0;
+var heightChoose = false;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const navBar = document.querySelector("nav");
+var cardImg = document.querySelectorAll(".cardImg");
+var cardImgCursor = 0;
+var sectionTwo = document.querySelectorAll(".sectionTwo");
+
+var AnimInfoGauche = document.querySelector("#AnimInfoGauche");
+var AnimInfoDroite = document.querySelector("#AnimInfoDroite");
+
+var terminalBloc = document.querySelectorAll(".terminalBloc");
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-document.querySelectorAll(".cardImg").forEach((img) => {
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ -";
+const intervals = {};
+
+document.querySelectorAll(".resume").forEach((elem) => {
+    const targetText = elem.innerText;
+    const words = targetText.split(" ");
+    let scrambledWords = [];
+
+    words.forEach((word, wordIndex) => {
+        let iteration = 0;
+        let scrambledWord = "";
+
+        const interval = setInterval(() => {
+            scrambledWord = word.split("").map((letter, index) => {
+                if (index < iteration) {
+                    return word[index];
+                }
+                return letters[Math.floor(Math.random() * letters.length)];
+            }).join("");
+
+            scrambledWords[wordIndex] = scrambledWord;
+            elem.innerText = scrambledWords.join(" ");
+
+            if (iteration >= word.length) {
+                clearInterval(interval);
+            }
+
+            iteration += 1 / 3;
+        }, 50);
+    });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+cardImg.forEach(function () {
     gsap.fromTo(
-        img,
+        ".cardImg",
         {
             rotateX: 30,
             rotateY: -90,
@@ -15,80 +84,337 @@ document.querySelectorAll(".cardImg").forEach((img) => {
             rotateX: 0,
             rotateY: 0,
             scrollTrigger: {
-                trigger: img,
+                id: "cardImgFirst",
+                trigger: ".cardImg",
                 toggleActions: "restart none none reverse",
                 start: "top 100%",
-                end: "top 30%",
+                end: "top 50%",
                 scrub: true,
                 // markers: true
             }
         }
     );
 
-    // Second animation: rotateY to -180 and rotateZ to -90 when the image passes the top 10%
-    gsap.fromTo(img,
+    gsap.fromTo(
+        ".cardImg",
         {
             rotateY: 0,
             rotateZ: 0,
-            y: 0
+            y: 0,
         },
         {
             rotateY: 180,
             rotateZ: -90,
-            y: 400,
-            scale: 3,
+            y: ajustementCard,
+            height: windowWidth,
+            width: windowHeight,
             scrollTrigger: {
-                trigger: img,
-                start: "top 5%",
-                end: "top -50%",
+                id: "cardImgSecond",
+                trigger: ".cardImg",
+                start: "top 100px",
+                end: "top -30%",
                 scrub: true,
-                // markers: true,
+                //markers: true,
             }
         }
     );
 })
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const nav = document.querySelector("nav");
-
-document.querySelectorAll(".formation").forEach((formation) => {
-    gsap.fromTo(
-        nav,
-        {
-            y: 0,
-        },
-        {
-            y: 0,
-            scrollTrigger: {
-                trigger: formation,
-                toggleActions: "play none none reverse",
-                start: "top 50%",
-                end: "top 40%",
-                markers: true,
-            },
-        }
-    );
+ScrollTrigger.create({
+    id: "sticky",
+    trigger: ".imgVoyageurDiv",
+    start: "top 0px",
+    endTrigger: ".cardImg",
+    end: "top -30%",
+    pin: true,
+    // markers: true,
 });
 
-window.addEventListener("scroll", reveal);
+var startAnimation = "top 40%";
+var endAnimation = "top 10%";
 
-var windowHeight = window.innerHeight;
+document.querySelectorAll(".capteursectionTwo").forEach(function () {
+    gsap.from(
+        "#AnimInfoGauche, #AnimInfoDroite",
+        {
+            y: 200,
+            opacity: 0,
+            scrollTrigger: {
+                id: "cardImgSecond",
+                trigger: ".capteursectionTwo",
+                toggleActions: "restart none none reverse",
+                start: startAnimation,
+                end: endAnimation,
+                scrub: true,
+                // markers: true
+            }
+        }
+    );
 
-var formation = document.querySelectorAll(".formation")
-var formationTop = 0;
+    gsap.from(
+        ".terminalBloc, .galleryBackground",
+        {
+            opacity: 0,
+            scrollTrigger: {
+                trigger: ".capteursectionTwo",
+                toggleActions: "restart none none reverse",
+                start: startAnimation,
+                end: endAnimation,
+                scrub: true,
+                // markers: true
+            }
+        }
+    );
+
+    gsap.from(
+        "nav, #alerte",
+        {
+            y: -50,
+            opacity: 0,
+            scrollTrigger: {
+                trigger: ".capteursectionTwo",
+                toggleActions: "play none none reverse",
+                toggleActions: "restart none none reverse",
+                start: startAnimation,
+                end: endAnimation,
+                scrub: true,
+                // markers: true
+            }
+        }
+    );
+})
+
+document.querySelectorAll(".terminalBloc").forEach(function () {
+    gsap.from(
+        ".terminalBloc",
+        {
+            scrollTrigger: {
+                id: "terminalBloc",
+                trigger: ".terminalBloc",
+                toggleActions: "restart none none reverse",
+                start: "top 20%",
+                end: "top -30%",
+                scrub: true,
+                pin: true,
+                //markers: true
+            }
+        }
+    );
+})
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////// Expérience ///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let isWindowAbove1200 = window.innerWidth > 1200;
+
+document.querySelectorAll(".expProDetail").forEach((detail, index) => {
+    gsap.from(detail, {
+        scrollTrigger: {
+            trigger: detail,
+            start: "top 70%",
+            end: "bottom 25%",
+            toggleActions: "play reverse play reverse",
+            // markers: true
+        },
+        x: (index % 2 === 1 && isWindowAbove1200) ? -300 : 300,
+        opacity: 0,
+        duration: 0.7,
+    });
+});
+
+document.querySelectorAll(".exProligne").forEach((exProligne) => {
+
+    gsap.fromTo(
+        exProligne.querySelectorAll(".hrBlack")[0],
+        {
+            height: "0%",
+        }, {
+        height: "calc(100% - 40px)",
+        scrollTrigger: {
+            trigger: exProligne,
+            start: "top 65%",
+            end: "bottom 65%",
+            scrub: true,
+            // markers: true
+        },
+    });
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.querySelectorAll(".galleryBackground").forEach((elem) => {
+    gsap.to(
+        elem,
+        {
+            y: -1000,
+            scrollTrigger: {
+                id: "galleryBackground",
+                trigger: elem,
+                toggleActions: "restart none none reverse",
+                start: "top 80%",
+                end: "bottom -1000px",
+                scrub: true,
+                pin: true,
+                markers: true
+            }
+        }
+    );
+})
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const st = SplitText.create("p", { type: "chars", charsClass: "char" });
+
+st.chars.forEach((char) => {
+    gsap.set(char, { attr: { "data-content": char.innerHTML } });
+});
+
+const textBlock = document.querySelector(".scrabledWord");
+
+textBlock.onpointermove = (e) => {
+    st.chars.forEach((char) => {
+        const rect = char.getBoundingClientRect();
+        const cx = rect.left + rect.width / 2;
+        const cy = rect.top + rect.height / 2;
+        const dx = e.clientX - cx;
+        const dy = e.clientY - cy;
+        const dist = Math.sqrt(dx * dx + dy * dy);
+        if (dist < 50)
+            gsap.to(char, {
+                overwrite: true,
+                duration: 1.2 - dist / 100,
+                scrambleText: {
+                    text: char.dataset.content,
+                    chars: "",
+                    speed: 2,
+                },
+                ease: 'none'
+            });
+    });
+};
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////// Compétences ///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.querySelectorAll(".sectionBD").forEach((elem) => {
+    gsap.from(
+        elem,
+        {
+            rotateZ: 30,
+            xPercent: -50,
+            yPercent: 100,
+            scrollTrigger: {
+                id: "sectionBD",
+                trigger: elem,
+                toggleActions: "restart none none reverse",
+                start: "top 100%",
+                end: "top 0%",
+                scrub: true,
+                //markers: true
+            }
+        }
+    );
+})
+
+document.querySelectorAll(".sectionWeb").forEach((elem) => {
+    gsap.from(
+        elem,
+        {
+            rotateZ: -30,
+            xPercent: 50,
+            yPercent: 100,
+            scrollTrigger: {
+                id: "sectionWeb",
+                trigger: elem,
+                toggleActions: "restart none none reverse",
+                start: "top 100%",
+                end: "top 0%",
+                scrub: true,
+                //markers: true
+            }
+        }
+    );
+})
+
+document.querySelectorAll(".sectionDev").forEach((elem) => {
+    gsap.from(
+        elem,
+        {
+            rotateZ: 30,
+            xPercent: -50,
+            yPercent: 100,
+            scrollTrigger: {
+                id: "sectionBD",
+                trigger: elem,
+                toggleActions: "restart none none reverse",
+                start: "top 100%",
+                end: "top 0%",
+                scrub: true,
+                //markers: true
+            }
+        }
+    );
+})
+
+document.querySelectorAll(".sectionLangue").forEach((elem) => {
+    gsap.from(
+        elem,
+        {
+            rotateZ: -30,
+            xPercent: 50,
+            yPercent: 100,
+            scrollTrigger: {
+                id: "sectionBD",
+                trigger: elem,
+                toggleActions: "restart none none reverse",
+                start: "top 100%",
+                end: "top 0%",
+                scrub: true,
+                //markers: true
+            }
+        }
+    );
+})
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 function reveal() {
-    console.log(formationTop, windowHeight * 0.5)
-    formationTop = formation[0].getBoundingClientRect().top;
 
-    if (formationTop < windowHeight * 0.5) {
-        document.querySelector(".imgVoyageurDiv").classList.add("d-none");
-        document.querySelector(".acceuil").classList.add("d-none");
-        console.log("test");
+    if (cardImg[0].getBoundingClientRect().left == 0) {
+
+        // Enleve
+        document.querySelector(".imgVoyageurDiv").classList.add("v-hidden");
+        document.querySelector(".acceuil").classList.add("v-hidden");
+
+        document.querySelector("nav").classList.remove("fixHeader");
+        document.querySelector("#alerte").classList.remove("fixHeader");
+        document.querySelector("#AnimInfoGauche").classList.remove("fixHeader");
+        document.querySelector("#AnimInfoDroite").classList.remove("fixHeader");
+
+        // Met
+        document.querySelector(".sectionTwo").classList.remove("v-hidden");
+        document.querySelector(".capteursectionTwo").classList.remove("d-none");
     } else {
-        document.querySelector(".imgVoyageurDiv").classList.remove("d-none");
-        document.querySelector(".acceuil").classList.remove("d-none");
-        console.log("test back");
+
+        // Enleve
+        document.querySelector(".imgVoyageurDiv").classList.remove("v-hidden");
+        document.querySelector(".acceuil").classList.remove("v-hidden");
+
+        document.querySelector("nav").classList.add("fixHeader");
+        document.querySelector("#alerte").classList.add("fixHeader");
+        document.querySelector("#AnimInfoGauche").classList.add("fixHeader");
+        document.querySelector("#AnimInfoDroite").classList.add("fixHeader");
+
+        // Met
+        document.querySelector(".sectionTwo").classList.add("v-hidden");
+        document.querySelector(".capteursectionTwo").classList.remove("d-none");
     }
 }
 
